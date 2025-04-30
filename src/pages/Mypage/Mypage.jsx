4e89d55eb108016711a0662be7c617e90f5d2aa9
Jangwon Seo <mopyour33@gmail.com>
2025-04-30 22:08:29 +0900

    second commit

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./Mypage.style.css"
+import React, { useState } from 'react';
+import { Grid, Box, Button } from '@mui/material';
+import "./Mypage.style.css";
+import MypageProfile from './MypageComponet/MypageProfile';
 
 const Mypage = () => {
-  return (
-    <div>마이페이지</div>
-  )
-}
+  const [selectedMenu, setSelectedMenu] = useState('profile');
+  
 
-export default Mypage
+  return (
+    <Box className="mypage-wrapper">
+      <Box className="mypage-border">
+          
+          {/* 왼쪽 메뉴 영역 (1/3) */}
+            <Box className="mypage-left">
+              <Button 
+                fullWidth 
+                variant={selectedMenu === 'favorites' ? 'contained' : 'outlined'} 
+                onClick={() => setSelectedMenu('favorites')}
+                className="mypage-menu-button"
+              >
+                내가 찜한 뉴스 리스트
+              </Button>
+              <Button 
+                fullWidth 
+                variant={selectedMenu === 'interest' ? 'contained' : 'outlined'} 
+                onClick={() => setSelectedMenu('interest')}
+                className="mypage-menu-button"
+              >
+                내 주요 관심 뉴스 리스트
+              </Button>
+              <Button 
+                fullWidth 
+                variant={selectedMenu === 'profile' ? 'contained' : 'outlined'} 
+                onClick={() => setSelectedMenu('profile')}
+                className="mypage-menu-button"
+              >
+                내 정보 변경
+              </Button>
+            </Box>
+
+          {/* 오른쪽 콘텐츠 영역 (2/3) */}
+            <Box className="mypage-right">
+              {selectedMenu === 'favorites' && <div>내가 찜한 뉴스 리스트 보여주기</div>}
+              {selectedMenu === 'interest' && <div>내 주요 관심 뉴스 리스트 보여주기</div>}
+              {selectedMenu === 'profile' && <div><MypageProfile /></div>}
+            </Box>
+
+      </Box>
+    </Box>
+  );
+};
+
+export default Mypage;
