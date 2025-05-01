b76bba1a2f7ace0360a4f59570259f506a97f587
Doyoon <dlwhdgus482@naver.com>
2025-05-01 22:05:38 +0900

    리스트 페이지 코딩 2차

--- src/pages/Listpage/components/NewsCard/NewsCard.jsx
+++ src/pages/Listpage/components/NewsCard/NewsCard.jsx
@@ -3,14 +3,14 @@ import { Card, CardContent, CardMedia, Typography, Box } from "@mui/material";
 import "./NewsCard.style.css";
 
 const NewsCard = ({ image, category, title, description }) => {
-    console.log("NewsCard props:", { image, category, title, description });
   return (
     <Card sx={{ maxWidth: 345, borderRadius: 3 }}>
       <CardMedia component="img" height="180" image={image} alt={title} />
       <CardContent>
         <Box display="flex" alignItems="center" mb={1}>
-          <img src="/logo.png" alt="logo" width={20} height={20} style={{ marginRight: 8 }} />
-          <Typography variant="subtitle2" color="text.secondary">{category}</Typography>
+          <Typography variant="subtitle2" color="text.secondary">
+            {category}
+          </Typography>
         </Box>
         <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
           {title}
