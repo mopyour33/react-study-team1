5618ba2014bff5bb8261f75b76097ef36004c3f7
Jiyoung Kim <kjy0520@gmail.com>
2025-05-04 13:58:42 +0900

    수정중

---
+++ src/hooks/useCategoryNews.js
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/api";
+
+
+const fetchCategoryNews= ({ category })=>{
+    console.log("훅 카테고리")
+    console.log("요청 카테고리:", category);
+
+    return api.get(`/news?language=ko&category=${category}`)
+}
+
+
+export const useCategoryNewsQuery=({category})=>{
+    return useQuery({
+        queryKey: ['news-category', {category}],
+        queryFn: () => fetchCategoryNews({caterory}),
+        select: (result) => result.data.results,        
+    })
+}
+
+  
+
+export const useSearchMovieQuery=({keyword, page, genre, sort})=>{
+    return useQuery({
+        queryKey: ['movie-searh', {keyword, page, genre, sort}],
+        queryFn: ()=>fetchSearchMovie({keyword, page, genre, sort}),
+        select: (result)=>result.data
+
+    })
+}
