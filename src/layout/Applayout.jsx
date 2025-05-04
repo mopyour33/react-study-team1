548adfc926c61db03446386c71aa0f1d68dedfb5
Hyeri Yu <yhr8340@naver.com>
2025-05-04 18:27:32 +0900

    navbar 수정

--- src/layout/Applayout.jsx
+++ src/layout/Applayout.jsx
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Navbar,
-  Container,
-  Form,
-  FormControl,
-  Button,
-  Nav,
-} from "react-bootstrap";
+import { Navbar, Container, Button, Nav } from "react-bootstrap";
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import useSignupStore from "../stores/useSignupStore";
 import "./Applayout.style.css";
@@ -55,23 +48,24 @@ const Applayout = () => {
             <Navbar.Toggle aria-controls="main-navbar-nav" />
             <Navbar.Collapse id="main-navbar-nav">
               <Nav className="ms-auto align-items-lg-center">
-                <Form className="d-flex me-lg-3 my-2 my-lg-0">
-                  <FormControl
-                    type="search"
-                    placeholder="Search news..."
-                    className="me-1 custom-input"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-dark">Search</Button>
-                </Form>
-
                 {isLoggedIn ? (
                   <>
                     <Nav.Link
                       as="span"
                       onClick={() => {
+                        navigate("/news");
+                        setExpanded(false);
+                      }}
+                      style={{ cursor: "pointer" }}
+                    >
+                      리스트
+                    </Nav.Link>
+
+                    <Nav.Link
+                      as="span"
+                      onClick={() => {
                         navigate("/mypage", { state: { userId } });
-                        setExpanded(false); // 햄버거 메뉴 닫기용
+                        setExpanded(false);
                       }}
                       style={{ cursor: "pointer" }}
                     >
@@ -109,7 +103,7 @@ const Applayout = () => {
                           내 카테고리 뉴스
                         </Nav.Link>
                         <Nav.Link
-                          a="span"
+                          as="span"
                           onClick={() => {
                             window.dispatchEvent(
                               new CustomEvent("changeMypageMenu", {
@@ -128,8 +122,8 @@ const Applayout = () => {
                     <Nav.Link
                       as="span"
                       onClick={() => {
-                        handleLogout(); // 로그아웃 기능 실행
-                        setExpanded(false); // 햄버거 메뉴 닫기
+                        handleLogout();
+                        setExpanded(false);
                       }}
                       style={{ cursor: "pointer" }}
                     >
@@ -157,6 +151,7 @@ const Applayout = () => {
         </div>
       </div>
 
+      <div className="page-content"></div>
       <Outlet />
     </div>
   );
